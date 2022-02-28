1de9cfa20764af7e646057c73012a4ea1be5ee58
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-02-28 10:52:30 -0300

    feat(form): create search functionality

--- components/Form.js
+++ components/Form.js
@@ -1,7 +1,18 @@
 import Image from 'next/image';
 import { MicrophoneIcon, SearchIcon } from '@heroicons/react/solid';
+import { useRef } from 'react';
+import { useRouter } from 'next/router';
 
 function Form() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+    if (!term) return;
+    router.push(`/search?term=${term}`);
+  };
+
   return (
     <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
       <Image
@@ -15,12 +26,20 @@ function Form() {
     sm:max-w-xl lg:max-w-2xl"
       >
         <SearchIcon className="h-5 mr-3 text-gray-400" />
-        <input type="text" className="focus:outline-none flex-grow" />
+        <input
+          ref={searchInputRef}
+          type="text"
+          className="focus:outline-none flex-grow"
+        />
         <MicrophoneIcon className="h-5" />
       </div>
       <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-        <button className="btn">Google Search</button>
-        <button className="btn">I´m Felling Lucky</button>
+        <button onClick={search} className="btn">
+          Google Search
+        </button>
+        <button onClick={search} className="btn">
+          I´m Felling Lucky
+        </button>
       </div>
     </form>
   );
