dcee6d13f3a25eaa361459c56181ed984dad31a6
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-02-28 14:29:37 -0300

    feat(search): getting results from api

--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,38 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import HeaderSearch from '../components/HeaderSearch';
+import SearchResults from '../components/SearchResults';
+import response from '../response';
 
-function Search() {
+function Search({ results }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HeaderSearch />
+      <SearchResults results={results} />
     </div>
   );
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = false;
+  const startIndex = context.query.start || '0';
+  const data = useDummyData
+    ? response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}
+        &start=${startIndex}`
+      ).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
