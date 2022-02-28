666b840db85659236604186c967b7d7ce6cbcb90
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-02-28 11:41:31 -0300

    feat(pages): create search results page

---
+++ pages/search.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import HeaderSearch from '../components/HeaderSearch';
+
+function Search() {
+  return (
+    <div>
+      <Head>
+        <title>Search Results</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
+
+export default Search;
